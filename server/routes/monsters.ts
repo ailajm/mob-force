1d40bde517914400af8f18fc6707a8c59e93d820
cotymckinney <cotymckinney@outlook.com>
2023-05-26 09:25:51 -0400

    added try-catch block for error handling

--- server/routes/monsters.ts
+++ server/routes/monsters.ts
@@ -6,15 +6,26 @@ const router = express.Router();
 
 // Get all monsters
 router.get('/', async (req: Request, res: Response) => {
-  const monsters = await Monster.find();
-  res.json(monsters);
+  try {
+    const monsters = await Monster.find();
+    res.json(monsters);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // Add a new monster
 router.post('/', async (req: Request, res: Response) => {
+
   const newMonster = new Monster(req.body);
-  const savedMonster = await newMonster.save();
-  res.json(savedMonster);
+
+  try {
+    const savedMonster = await newMonster.save();
+    res.json(savedMonster);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
+// Export
 export default router;
